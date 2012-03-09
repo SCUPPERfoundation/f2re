f16323328773d17a556790ad28ec89a424876495
alan <alan>
2012-03-09 07:56:02 +0000

    Getting rid of the maps files on this kit - pt 2

--- maps/data.mk
+++
@@ -1,9 +0,0 @@
-# makefile with dependencies for fed2 map files 
-# include in local makefile
-
-$(DEST)/%.loc: ./%.loc; cp $< $@
-$(DEST)/%.npc: ./%.npc; cp $< $@
-$(DEST)/%.msg: ./%.msg; cp $< $@
-$(DEST)/%.txt: ./%.txt; cp $< $@
-$(DEST)/%.xev: ./%.xev; cp $< $@
-$(DEST)/%.xob: ./%.xob; cp $< $@
