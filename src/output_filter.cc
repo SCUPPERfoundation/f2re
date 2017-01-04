2175c30e27b33fdc811c9d682a5955615d1864ae
Alan <alan@ibgames.com>
2017-01-04 08:54:54 +0000

    Copyright notice updated

--- src/output_filter.cc
+++ src/output_filter.cc
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------
-		        Copyright (c) Alan Lenton 1985-2016
+		        Copyright (c) Alan Lenton 1985-2017
 	All Rights Reserved. No part of this software may be reproduced,
 	transmitted, transcribed, stored in a retrieval system, or translated
 	into any human or computer language, in any form or by any means,
