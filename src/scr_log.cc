2e8b96da92494e38cacce42c03f12fd40e3c7f29
Alan <alan@ibgames.com>
2019-03-28 11:36:11 +0000

    Changed licence to GPL v2

--- src/scr_log.cc
+++ src/scr_log.cc
@@ -1,10 +1,22 @@
 /*-----------------------------------------------------------------------
-		Copyright (c) Alan Lenton 1985-2017
-	All Rights Reserved. No part of this software may be reproduced,
-	transmitted, transcribed, stored in a retrieval system, or translated
-	into any human or computer language, in any form or by any means,
-	electronic, mechanical, magnetic, optical, manual or otherwise,
-	without the express written permission of the copyright holder.
+                          Federation 2
+              Copyright (c) 1985-2018 Alan Lenton
+
+This program is free software: you can redistribute it and /or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation: either version 2 of the License, or (at 
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY: without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+
+You can find the full text of the GNU General Public Licence at
+           http://www.gnu.org/copyleft/gpl.html
+
+Programming and design:     Alan Lenton (email: alan@ibgames.com)
+Home website:                   www.ibgames.net/alan
 -----------------------------------------------------------------------*/
 
 #include "scr_log.h"
